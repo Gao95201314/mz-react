321c9681574693fc8fc97065f3fba3461b8292da
gaojinhong <2359602415@qq.com>
2019-01-06 21:22:38 +0800

    9.9拼团页面完成

---
+++ src/views/Home/HomeGroup/index.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import LazyLoad from 'react-lazyload';
+import './index.scss';
+class HomeGroup extends Component {
+  render() {
+    return (
+      <div className="gao-lazy">
+        <LazyLoad height={200}>
+          <img src="https://mall.s.maizuo.com/8bcd915edeea67f58cbfa90d264b8319.jpg?x-oss-process=image/quality,Q_70" alt="xixi" />
+        </LazyLoad>
+        <div className="gao-lazy-detail">
+          <p>广西融安金桔 3斤装</p>
+          <p>基地果园现摘现发 汁水丰满 皮滑脆甜不麻嘴</p>
+        </div>
+        <div className="gao-lazy-ping">
+          <p>￥52<span>原价￥65.9</span></p>
+          <p>去拼团</p>
+        </div>
+        <LazyLoad height={200}>
+          <img src="https://mall.s.maizuo.com/005e2ca405e37e499455139686640ed7.jpg?x-oss-process=image/quality,Q_70" alt="xixi" />
+
+        </LazyLoad>
+        <div className="gao-lazy-detail">
+          <p>广西融安金桔 3斤装</p>
+          <p>基地果园现摘现发 汁水丰满 皮滑脆甜不麻嘴</p>
+        </div>
+        <div className="gao-lazy-ping">
+          <p>￥52<span>原价￥65.9</span></p>
+          <p>去拼团</p>
+        </div>
+        <LazyLoad height={200}>
+          <img src="https://mall.s.maizuo.com/9c98f57dcae3a2e00bbad1bd1a4a6742.jpg?x-oss-process=image/quality,Q_70" alt="xixi" />
+        </LazyLoad>
+        <div className="gao-lazy-detail">
+          <p>广西融安金桔 3斤装</p>
+          <p>基地果园现摘现发 汁水丰满 皮滑脆甜不麻嘴</p>
+        </div>
+        <div className="gao-lazy-ping">
+          <p>￥52<span>原价￥65.9</span></p>
+          <p>去拼团</p>
+        </div>
+        <LazyLoad height={200}>
+          <img src="https://mall.s.maizuo.com/57f0021c164efc982ae2361b159e77cb.jpg?x-oss-process=image/quality,Q_70" alt="xixi" />
+        </LazyLoad>
+        <div className="gao-lazy-detail">
+          <p>广西融安金桔 3斤装</p>
+          <p>基地果园现摘现发 汁水丰满 皮滑脆甜不麻嘴</p>
+        </div>
+        <div className="gao-lazy-ping">
+          <p>￥52<span>原价￥65.9</span></p>
+          <p>去拼团</p>
+        </div>
+        <LazyLoad height={200}>
+          <img src="https://mall.s.maizuo.com/86da9fae8dcea902ce349c602f8bf651.jpg?x-oss-process=image/quality,Q_70" alt="xixi" />
+
+        </LazyLoad>
+        <div className="gao-lazy-detail">
+          <p>广西融安金桔 3斤装</p>
+          <p>基地果园现摘现发 汁水丰满 皮滑脆甜不麻嘴</p>
+        </div>
+        <div className="gao-lazy-ping">
+          <p>￥52<span>原价￥65.9</span></p>
+          <p>去拼团</p>
+        </div>
+        <LazyLoad height={200}>
+          <img src="https://mall.s.maizuo.com/b2331ed827c7e5b307c40f8e08ddbbc7.jpg?x-oss-process=image/quality,Q_70" alt="xixi" />
+
+        </LazyLoad>
+        <div className="gao-lazy-detail">
+          <p>广西融安金桔 3斤装</p>
+          <p>基地果园现摘现发 汁水丰满 皮滑脆甜不麻嘴</p>
+        </div>
+        <div className="gao-lazy-ping">
+          <p>￥52<span>原价￥65.9</span></p>
+          <p>去拼团</p>
+        </div>
+        <LazyLoad height={200}>
+          <img src="https://mall.s.maizuo.com/ac7dc94310ad59a9121fcf93e30fd3c5.jpg?x-oss-process=image/quality,Q_70" alt="xixi" />
+
+        </LazyLoad>
+        <div className="gao-lazy-detail">
+          <p>广西融安金桔 3斤装</p>
+          <p>基地果园现摘现发 汁水丰满 皮滑脆甜不麻嘴</p>
+        </div>
+        <div className="gao-lazy-ping">
+          <p>￥52<span>原价￥65.9</span></p>
+          <p>去拼团</p>
+        </div>
+        <LazyLoad height={200}>
+          <img src="https://mall.s.maizuo.com/f7f7c470111b9e369e7546348ca9ef8b.jpg?x-oss-process=image/quality,Q_70" alt="xixi" />
+
+        </LazyLoad>
+        <div className="gao-lazy-detail">
+          <p>广西融安金桔 3斤装</p>
+          <p>基地果园现摘现发 汁水丰满 皮滑脆甜不麻嘴</p>
+        </div>
+        <div className="gao-lazy-ping">
+          <p>￥52<span>原价￥65.9</span></p>
+          <p>去拼团</p>
+        </div>
+      </div>
+    )
+  }
+}
+export default HomeGroup;
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react';
 import Navbar from '@/components/Navbar';
-import {Switch,Route,Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import HomeCenter from '@/views/Home/HomeCenter';
 import HomeCinemas from '@/views/Home/HomeCinemas';
 import HomeFilms from '@/views/Home/HomeFilms';
-import Todo from '@/views/Home/Todo';
+import HomeGroup from '@/views/Home/HomeGroup';
 
-export default class Home extends Component{
-  constructor(props){
+export default class Home extends Component {
+  constructor(props) {
     super(props);
 
-    this.state={
-      NavTabs:[
-        { key: 'films', href: "/films", icon: "icon-films", name: '电影' },
-        { key: 'cinemas', href: "/cinemas", icon: "icon-cinemas", name: '影院' },
-        { key: 'todo', href: "/todo", icon: "icon-cinemas", name: 'Todo' },
-        { key: 'center', href: "/center", icon: "icon-center", name: '我的' }
+    this.state = {
+      NavTabs: [
+        { key: 'films', href: "/films", icon: "icon-dianying", name: '电影' },
+        { key: 'cinemas', href: "/cinemas", icon: "icon-yingyuan", name: '影院' },
+        { key: 'group', href: "/group", icon: "icon-pintuan", name: '9.9拼团' },
+        { key: 'center', href: "/center", icon: "icon-wode-copy", name: '我的' }
       ]
     }
   }
-  render(){
+  render() {
     return (
       <div>
-         <Switch>
+        <Switch>
           <Route path="/films" component={HomeFilms}></Route>
           <Route path="/cinemas" component={HomeCinemas}></Route>
-          <Route path="/todo" component={Todo}></Route>
+          <Route path="/group" component={HomeGroup}></Route>
           <Route path="/center" component={HomeCenter}></Route>
           <Redirect to="/films"></Redirect>
         </Switch>
-         <Navbar tabs={this.state.NavTabs}></Navbar>
+        <Navbar tabs={this.state.NavTabs}></Navbar>
       </div>
     )
   }
